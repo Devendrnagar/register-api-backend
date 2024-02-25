e010b7ba0970113e07c58676d949c5ffccf5bdb9
devendra nagar <devendranagar853@gmail.com>
2024-02-25 08:50:50 +0530

    from field change

--- models/From.js
+++ models/From.js
@@ -1,10 +1,10 @@
 const mongoose =require('mongoose');
 
 const FromSchema = new mongoose.Schema({
-    name:String,
-    userName:String,
-    email:String,
-    password:String
+    name:{ type: String, required: true, trim: true },
+    userName:{ type: String, required: true, trim: true },
+    email:{ type: String, required: true, trim: true },
+    password:{ type: String, required: true, trim: true }
 })
 const FromModel = mongoose.model("data",FromSchema)
 module.exports=FromModel;
